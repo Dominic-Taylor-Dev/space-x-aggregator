41b689c052495c40a04238cc52b08f47bd75fab6
Dominic Taylor <email.dom.taylor@gmail.com>
2023-10-27 01:16:09 +0100

    Add error handling content
    - Add a sentence to tell the user to refresh or contact support
    - This is largely a placeholder rather than a final implementation that is production-ready

--- client/src/components/launch-list/LaunchList.tsx
+++ client/src/components/launch-list/LaunchList.tsx
@@ -4,17 +4,38 @@ import style from "./LaunchList.module.css";
 import { Launch } from "../../types";
 import { getAggregatedLaunchData } from "../../apis/launches";
 import Spinner from "../spinner/Spinner";
+import { ApiError } from "../../apis/http";
 
 const LaunchList: React.FC = () => {
   const [launches, setLaunches] = useState<Launch[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<ApiError | null>(null);
 
   useEffect(() => {
-    getAggregatedLaunchData().then((launchinfo) => {
-      setLaunches(launchinfo.data.results);
-      setLoading(false);
-    });
+    getAggregatedLaunchData()
+      .then((launchinfo) => {
+        setLaunches(launchinfo.data.results);
+        setLoading(false);
+      })
+      .catch((error: ApiError) => {
+        setError(error);
+        setLoading(false);
+      });
   }, []);
+
+  if (error) {
+    return (
+      <p className={style["error-text"]} role="alert" aria-live="assertive">
+        We are sorry but something went wrong with your request. Please refresh
+        the page
+        {error.data
+          ? ` or contact user support, quoting error reference:
+        ${error.data.referenceCode}`
+          : ""}
+        .
+      </p>
+    );
+  }
 
   if (loading) {
     return (
